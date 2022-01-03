0b465a3c52924e36cc4604e286ba6f8fad80102c
karlo <karlohrvacic@gmail.com>
2022-01-03 15:39:12 +0100

    Pause redirect

--- public/js/errorPages.js
+++ public/js/errorPages.js
@@ -10,14 +10,14 @@ let paused = false;
 
 let duration = 4
 let timer = setInterval(() => {
-    if(duration <= 0){
+    if (!paused){
+        if(duration <= 0){
         window.location.href = "/";
         clearInterval(timer)
-    }
-    document.getElementById("number").innerHTML = duration;
-    if (!paused){
-        duration--;
-    }
+        }
+        document.getElementById("number").innerHTML = duration;
+            duration--;
+        }
     }, 1000);
 
 function pause(){
